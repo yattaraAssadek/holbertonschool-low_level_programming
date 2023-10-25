db3b20928c620f05693427ccc29e14e184596b72
yattaraAssadek <yattaraassadek0@gmail.com>
2023-10-25 02:17:16 -0700

    task

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -15,7 +15,6 @@ void rev_string(char *s)
 	for (i = 0; i < len / 2; i++)
 {
 		tmp = *(s + i);
-		*(s + i) = *(s + len - i - 1);							  *(s + len - i - 1) = tmp;
-}
+		*(s + len - i - 1) = tmp;
 }
 
