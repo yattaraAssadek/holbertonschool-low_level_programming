f0db24fb8f74960510d2aceadc65997346d37150
yattaraAssadek <yattaraassadek0@gmail.com>
2023-10-29 11:42:30 -0700

    task

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -5,7 +5,6 @@
  * print_diagsums - prints the sum of the two diagonals of a square matrix
  * @a: array to be used
  * @size: size of the matrix
- *
  * Return: void
  */
 
@@ -14,10 +13,10 @@ void print_diagsums(int *a, int size)
 	int i = 0, j = 0, sum_1 = 0, sum_2 = 0;
 
 	for (; i < size * size; i += size)
-	{
-		sum_1 += a[i + j];
-		sum_2 += a[i + (size - 1) - j];
-		j++;
-	}
+{
+	sum_1 += a[i + j];
+	sum_2 += a[i + (size - 1) - j];
+	j++;
+}
 	printf("%d, %d\n", sum_1, sum_2);
 }
